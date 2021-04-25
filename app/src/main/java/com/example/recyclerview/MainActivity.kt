4d57ed3484f592dec2a5f0621df6dd28151649ed
KYOHEI-jp <kingdomhearts510@gmail.com>
2021-04-25 14:44:12 +0900

    finished

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var titleList = mutableListOf<String>()
+    private var descList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        postToList()
+
+        rv_recyclerView.layoutManager = LinearLayoutManager(this)
+        rv_recyclerView.adapter = RecyclerAdapter(titleList, descList, imagesList)
+    }
+
+    private fun addToList(title: String, description: String, image: Int) {
+        titleList.add(title)
+        descList.add(description)
+        imagesList.add(image)
+    }
+
+    private fun postToList() {
+        for (i in 1..25) {
+            addToList("Title $i", "Description $i", R.mipmap.ic_launcher)
+        }
     }
 }
--- app/src/main/java/com/example/recyclerview/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerview/RecyclerAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.recyclerview
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,19 +13,29 @@ class RecyclerAdapter (private var titles: List<String>, private var details: Li
                       private var images: List<Int>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemTitle: TextView = itemView.findViewById(R.id.tv_title)
+        val itemDetail: TextView = itemView.findViewById(R.id.tv_description)
+        val itemPicture: ImageView = itemView.findViewById(R.id.iv_image)
 
+        init {
+            itemView.setOnClickListener { v: View ->
+                val position: Int = absoluteAdapterPosition
+                Toast.makeText(itemView.context, "${position + 1}がクリックされました。", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(v)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = titles.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemTitle.text = titles[position]
+        holder.itemDetail.text = titles[position]
+        holder.itemPicture.setImageResource(images[position])
     }
 
 
